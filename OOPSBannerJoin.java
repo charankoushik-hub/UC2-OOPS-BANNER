2195022352e0b3fd3b740624c68adc80f693d5a4
charankoushik-hub <charankoushiksanchi@gmail.com>
2026-03-09 22:59:36 +0530

    Add files via upload

---
+++ OOPSBannerJoin.java
@@ -0,0 +1,11 @@
+public class OOPSBannerJoin {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", " *** ", " *** ", "*****", "*****"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*   *", "*"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*****", "****"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*", "*"));
+        System.out.println(String.join(" ", " *** ", " *** ", "*", "*****"));
+
+    }
+}
